c68a027704075e6244b6d2f3cd5820f9352cf974
Ram1lia <116273468+Ram1lia@users.noreply.github.com>
2023-03-07 19:17:04 +0600

    Update admin.py

--- final/product/admin.py
+++ final/product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from products.models import Product, Brand, Type, Category
 
-from .models import Product
-
+admin.site.register(Brand)
 admin.site.register(Product)
-# Register your models here.
+admin.site.register(Type)
+admin.site.register(Category)
