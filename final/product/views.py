391add27911926e100c58e9fafad2d8272ff0a6f
Ram1lia <116273468+Ram1lia@users.noreply.github.com>
2023-03-07 19:27:05 +0600

    Update views.py

--- final/product/views.py
+++ final/product/views.py
@@ -1,21 +1,14 @@
-from rest_framework import generics
-from .models import *
+from rest_framework.decorators import api_view
+from rest_framework.filters import SearchFilter
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_POST
+from rest_framework import status, viewsets
+from products.models import Product, Category
+from products.serializers import ProductListSerializer, ProductDateilListSerializer
 
-from .models import Product
-from .serializers import ProductSerializer, ProductDetailSerializer
 
-from .filters import ProductFilters
-from django_filters.rest_framework import DjangoFilterBackend
-
-
-class ProductView(generics.ListAPIView):
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    filter_backends = (DjangoFilterBackend, )
-    filterset_class = ProductFilters
-
-
-class ProductDetail(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductDetailSerializer
-
+    serializer_class = ProductListSerializer
